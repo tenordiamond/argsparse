087ba1e308c9063dbff96434677474414faebc41
tenordiamond <christer.hellkvist@gmail.com>
2017-12-31 12:38:21 +0100

    reverserd the methods

--- convert_temp.py
+++ convert_temp.py
@@ -6,11 +6,12 @@ import argparse
 
 #converts to fahrenheit
 def fahr(x):
-    x = (x - 32) / 1.8
+    x = (x * 1.8) + 32
     return x
+
 #converts to celsius
 def cel(x):
-    x = (x * 1.8) + 32
+    x = (x - 32) / 1.8
     return x
 
 # I create a ArgumentParser object
@@ -18,8 +19,8 @@ parser = argparse.ArgumentParser(description='Converts temperatures between Cels
 
 # Then I add my arguments and tell the parser how to take the
 # strings from the command line and turn them into objects
-parser.add_argument('integer', metavar='N', type=int, help='An integer to convert to either celsius or fahrenheit')
-# I add arguments 
+parser.add_argument('integer', metavar='N', type=float, help='An integer to convert to either celsius or fahrenheit')
+# I add arguments
 parser.add_argument('--toCels', dest='converted', action='store_const', const=cel, default=fahr, help='Converts an integer between celsius and fahrenheit (default: to fahrenheit)')
 
 
